b87e5a2c7ee23c77249544686b5c120004603ce8
majdanrc <rmarkiew@gmail.com>
2024-01-30 22:17:56 +0100

    nav prettier

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,63 +1,96 @@
 "use client";
 
-import React from "react";
-import { BsSearch } from "react-icons/bs";
-import { ImCart } from "react-icons/im";
-import { FiMenu } from "react-icons/fi";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { monoton } from "@/app/ui/fonts";
-import { useState } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  const [navOpen, setNavOpen] = useState(false);
-  const handleNav = () => {
-    setNavOpen(!navOpen);
-  };
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      capt: "home",
+      link: "",
+    },
+    {
+      id: 2,
+      capt: "o kinie",
+      link: "about",
+    },
+    {
+      id: 3,
+      capt: "portfolio",
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      capt: "experience",
+      link: "experience",
+    },
+    {
+      id: 5,
+      capt: "contact",
+      link: "contact",
+    },
+  ];
 
   return (
-    <nav className="fixed w-full h-20 shadow-xl bg-gray-900">
-      <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
-        <div className="flex h-full items-center">
-          <Link href="/">
-            <Image
-              src="/logo.png"
-              alt="logo"
-              width={80}
-              height={80}
-              className="cursor-pointer relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            />
-          </Link>
-          <Link href="/">
-            <h1
-              className={`${monoton.className} text-xl md:text-4xl md:leading-normal text-newa-green`}
-            >
-              kino newa
-            </h1>
-          </Link>
-        </div>
-        <div className="hidden sm:flex">
-          <ul className="hidden sm:flex">
-            <Link href="/about">
-              <li className="ml-10 uppercase hover:border-b text-xl">
-                O Kinie
-              </li>
-            </Link>
-          </ul>
-          <ul className="hidden sm:flex">
-            <Link href="/news">
-              <li className="ml-10 uppercase hover:bg-newa-green text-xl">
-                Aktualności
-              </li>
-            </Link>
-          </ul>
-        </div>
-        <div onClick={handleNav} className="md:hidden cursor-pointer pl-24">
-          <AiOutlineMenu size={25} />
-        </div>
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
+      <div className="flex h-full items-center">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={80}
+            height={80}
+            className="cursor-pointer relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          />
+        </Link>
+        <Link href="/">
+          <h1
+            className={`${monoton.className} text-xl md:text-4xl md:leading-normal text-newa-green`}
+          >
+            kino newa
+          </h1>
+        </Link>
       </div>
-    </nav>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, capt, link }) => (
+          <li
+            key={id}
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-300 hover:scale-105 hover:text-newa-green duration-200 link-underline"
+          >
+            <Link href={link}>{capt}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setNav(!nav)} href={link}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
